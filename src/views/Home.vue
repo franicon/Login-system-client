095c09b39b0de96ba3ba19fee641139ad3603a38
Abayomi Aluko <franicons@gmail.com>
2022-12-31 14:36:51 +0100

    dev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,25 @@
 <template>
-  <div></div>
+  <div class="bg-[#EAE6F6] min-h-screen flex items-center justify-center">
+    <div
+      class="flex flex-col md:flex-row space-y-6 bg-white rounded-2xl shadow-2xl shadow-[#8B8CB2]"
+    >
+      <!--left-->
+      <div
+        class="bg-[#1E1E64] text-white space-y-3 md:max-w-xs p-10 md:rounded-l-2xl"
+      >
+        <h3 class="text-2xl font-medium space-y-4">
+          Implement auth user with ease
+        </h3>
+        <p class="text-sm text-[#8B8CB2]">
+          Guide your customer on a fantastic journey within your web app
+        </p>
+      </div>
+      <!--right-->
+      <div class="p-40">
+        <h2 class="">Create Account</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
